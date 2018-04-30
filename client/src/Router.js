b6ad172ed10144a4a814c4887189ea824be51e35
Kyle Booth <kbooth1000@gmail.com>
2018-04-30 15:35:48 -0400

    Change MainRoutes to Router

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import HomeScreen from './HomeScreen';
+import AccountScreen from './AccountScreen';
+import CategoryScreen from './CategoryScreen';
+import IngredientsScreen from './IngredientsScreen';
+import RecipesList from './RecipesList';
+import RecipeScreen from './RecipeScreen';
 
-let RouterComp = () =>
-  <Router>
-      <Route exact path="/" component={App} />
-  </Router>
+// import Category from './Category';
 
-export default Router;
+let Router = () =>
+    <div>
+        <Switch>
+            <Route path="/" component={HomeScreen} />
+            <Route path="/account" component={AccountScreen} />
+            <Route path="/categories" component={CategoryScreen} />
+            <Route path="/ingredients" component={IngredientsScreen} />
+            <Route path="/recipes" component={RecipesScreen} />
+            <Route path="/#/recipes/:recipe" component={RecipeScreen} />
+        </Switch>
+  </div>;
+
+  export default Router;
