7ef880e097bafee625c0d0d75ea08d60b77889d9
Robby Ackerley <rcackerley@me.com>
2018-04-30 14:39:57 -0400

    fixed bugs

---
+++ client/src/components/authentication/CreateAccount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormSubmissionButton from './FormSubmissionButton';
+
+let CreateAccount = () =>
+  <div>
+  <CreateAccountForm />
+  <FormSubmissionButton />
+  </div>
+
+
+export default CreateAccount;
---
+++ client/src/components/authentication/CreateAccountForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let CreateAccountForm = () =>
+  <form>
+    <h3>Create Account</h3>
+    <input placeholder="first name" />
+    <input placeholder="last name" />
+    <input placeholder="username" />
+    <input placeholder="email" />
+    <input placeholder="password" />
+  </form>
+
+export default CreateAccountForm
