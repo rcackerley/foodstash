4713b724919e196e5bc59869dec1dc027710bed2
Robby Ackerley <rcackerley@me.com>
2018-04-30 11:54:16 -0400

    Set up router and base store

---
+++ client/src/reducers/index.js
@@ -0,0 +1,15 @@
+
+
+const initialState = {};
+
+
+const reducerRoutes = {
+  default: (state, action) => state
+}
+
+const rootReducer = (state = initialState, action) => {
+  let reducerAction = reducerRoutes[action.type] || reducerRoutes.default
+  return reducerAction(state, action)
+}
+
+export default rootReducer;
