23dc519f3889ff2b5229caf5a0b24dfc34f44ed4
Robby Ackerley <rcackerley@me.com>
2018-04-30 14:08:15 -0400

    Fixed import bugs for app to compile

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-let Router = () =>
+let RouterComp = () =>
   <Router>
       <Route exact path="/" component={App} />
   </Router>
