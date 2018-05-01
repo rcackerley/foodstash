ab46449e66d0d51fbc257a686290e3b22a075025
James McDowell <jamesnmcdowell@gmail.com>
2018-05-01 11:28:02 -0400

    fix bugs

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css';
 import Router from './Router';
 import {Provider} from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
