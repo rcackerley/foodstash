2a376cd7658f2c194e1a975ecb7f7d7b9eaaf1b2
James McDowell <jamesnmcdowell@gmail.com>
2018-05-02 17:27:58 -0400

    fix router bug

--- client/src/Router.js
+++ client/src/Router.js
@@ -8,11 +8,8 @@ import AccountScreen from './components/AccountScreen';
 import CategoryListScreen from './components/CategoryListScreen';
 import IngredientListScreen from './components/IngredientListScreen';
 import RecipeListScreen from './components/RecipeListScreen';
-import SearchScreen from './components/SearchScreen';
-import SearchResultsScreen from './components/SearchResultsScreen';
 import RecipeScreen from './components/RecipeScreen';
 import AddDerivedRecipeForm from './components/AddDerivedRecipeForm';
-import RecipeScreen from './components/RecipeScreen';
 import AddScreen from './components/AddScreen';
 
 let Router = () =>
@@ -23,7 +20,6 @@ let Router = () =>
             <Route path="/recipes" exact component={RecipeListScreen} />
             <Route path="/recipes/1" component={RecipeScreen} />  
             <Route path="/account" component={AccountScreen} />  
-            <Route path="/search-results" component={SearchResultsScreen} /
             <Route path="/categories" component={CategoryListScreen} />
             <Route path="/ingredients" component={IngredientListScreen}/>
             <Route path="/account" component={AccountScreen} />
