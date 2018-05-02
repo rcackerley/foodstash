4a21892137273eb46786d456b9c4672a12534a9e
James McDowell <jamesnmcdowell@gmail.com>
2018-05-02 16:06:30 -0400

    Add recipe screeen

--- client/src/Router.js
+++ client/src/Router.js
@@ -11,13 +11,15 @@ import PrimaryNav from './components/PrimaryNav';
 import SecondaryNav from './components/PrimaryNav';
 import RecipeListScreen from './components/RecipeListScreen';
 import SearchScreen from './components/SearchScreen';
+import RecipeScreen from './components/RecipeScreen';
 
 let Router = () =>
   <HashRouter>
         <Switch>
             <Route path="/categories" component={CategoryListScreen} />  
             <Route path="/ingredients" component={IngredientListScreen}/> 
-            <Route path="/recipes" exact component={RecipeListScreen} />   
+            <Route path="/recipes" exact component={RecipeListScreen} />
+            <Route path="/recipes/1" component={RecipeScreen} />  
             <Route path="/account" component={AccountScreen} />  
             <Route path="/recipes" component={RecipeListScreen} />
             <Route path="/#/recipes/:recipe"  />
