ca2b5f3dfbb6d04bcefede501fde4e6b98ee1af7
Robby Ackerley <rcackerley@me.com>
2018-05-03 13:12:04 -0400

    Added login and loading token from local storage

--- client/src/Router.js
+++ client/src/Router.js
@@ -13,17 +13,28 @@ import AddDerivedRecipeForm from './components/AddDerivedRecipeForm';
 import AddScreen from './components/AddScreen';
 import RegisterScreen from './components/RegisterScreen';
 import LoginScreen from './components/LoginScreen';
+import {connect} from 'react-redux';
+import {setToken} from './actions/index';
+import {getTokenFromLocalStorage} from './lib/index';
 
-let Router = () =>
-  <HashRouter>
+class Router extends React.Component {
+  componentDidMount() {
+    let {setToken} = this.props;
+    let token = getTokenFromLocalStorage();
+    setToken(token)
+  }
+
+  render() {
+    return (
+      <HashRouter>
         <Switch>
-            <Route path="/register" component={RegisterScreen} />  
-            <Route path="/login" component={LoginScreen} />  
-            <Route path="/categories" component={CategoryListScreen} />  
-            <Route path="/ingredients" component={IngredientListScreen}/> 
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/categories" component={CategoryListScreen} />
+            <Route path="/ingredients" component={IngredientListScreen}/>
             <Route path="/recipes" exact component={RecipeListScreen} />
-            <Route path="/recipes/1" component={RecipeScreen} />  
-            <Route path="/account" component={AccountScreen} />  
+            <Route path="/recipes/1" component={RecipeScreen} />
+            <Route path="/account" component={AccountScreen} />
             <Route path="/categories" component={CategoryListScreen} />
             <Route path="/ingredients" component={IngredientListScreen}/>
             <Route path="/account" component={AccountScreen} />
@@ -31,6 +42,12 @@ let Router = () =>
             <Route path="/add-derived" component={AddDerivedRecipeForm} />
             <Route path="/#/recipes/:recipe" />
         </Switch>
-</HashRouter>;
+      </HashRouter>
+    )
+  }
+}
 
-export default Router;
+let mapDispatchToProps = dispatch => ({setToken: (token) => dispatch(setToken(token)) });
+let mapStateToProps = state => state;
+let RouterContainer = connect(mapStateToProps, mapDispatchToProps)(Router)
+export default RouterContainer;
