198c4bd29f35c2e7bdd19e654816a84a3ffbf3c2
Robby Ackerley <rcackerley@me.com>
2018-05-03 12:25:41 -0400

    Add create account

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -11,5 +11,12 @@ export const addRecipe = recipe =>
     payload: recipe
   })
 
+export const setToken = token =>
+  ({
+    type: setToken.toString(),
+    payload: token
+  })
+
 addRecipe.toString = () => 'ADD_RECIPE';
 setSearchResultRecipes.toString = () => 'SET_SEARCH_RESULTS_RECIPES';
+setToken.toString = () => 'SET_TOKEN';
