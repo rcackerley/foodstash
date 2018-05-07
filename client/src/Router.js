5cdc614cfa77b6dc038bc0df3b03256ff61232e9
James McDowell <jamesnmcdowell@gmail.com>
2018-05-07 13:24:34 -0400

    fix router change

--- client/src/Router.js
+++ client/src/Router.js
@@ -19,7 +19,7 @@ let Router = () =>
       <HashRouter>
         <Switch>
             <Route path="/register" component={RegisterScreen} />
-            <Route path="/" component={LoginScreen} />
+            <PrivateRoute path="/" exact component={CategoryListScreen} />
             <Route path="/login" component={LoginScreen} />
             <PrivateRoute path="/ingredients" component={IngredientListScreen}/>
             <PrivateRoute path="/recipes/:type/:id" exact component={RecipeListScreen} />
